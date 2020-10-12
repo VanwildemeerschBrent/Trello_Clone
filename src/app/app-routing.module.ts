ca3afbdb8220684f31a9429da6e495fa4436696b
Brent Vanwildemeersch <vanwildemeersch.brent@gmail.com>
2020-10-12 15:51:35 +0200

    Routing & footer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { BoardOverviewComponent } from './modules/boardOverview/board-overview/board-overview.component';
 import { LoginComponent } from './modules/login/components/login/login.component';
+import { HomeComponent } from './shared/components/home/home.component';
 
 const routes: Routes = [
   { path: 'auth/login', component: LoginComponent },
-  { path: 'overview', component: BoardOverviewComponent }
+  {
+    path: '', component: HomeComponent, children: [
+      { path: 'overview', component: BoardOverviewComponent }
+    ]
+  }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,27 +3,31 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './shared/components/header/header.component';
 import { FooterComponent } from './shared/components/footer/footer.component';
 import { LoginComponent } from './modules/login/components/login/login.component';
 import { RegistrationComponent } from './modules/registration/components/registration/registration.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { HomeComponent } from './shared/components/home/home.component';
+import { HeaderComponent } from './shared/components/header/header.component';
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
     FooterComponent,
     LoginComponent,
-    RegistrationComponent
+    RegistrationComponent,
+    HomeComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    RouterModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
