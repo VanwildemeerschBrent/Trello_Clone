fd96955d7a10cfbacd21dc2e67605e6268274e64
Brent Vanwildemeersch <vanwildemeersch.brent@gmail.com>
2020-10-14 11:17:47 +0200

    Update Auto routing to overview

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,12 +7,14 @@ import { HomeComponent } from './shared/components/home/home.component';
 
 const routes: Routes = [
   { path: 'auth/login', component: LoginComponent },
+  { path: '', redirectTo: 'main/overview', pathMatch: 'full' },
   {
-    path: '', component: HomeComponent, children: [
+    path: 'main', component: HomeComponent, children: [
       { path: 'overview', component: BoardOverviewComponent },
       { path: 'board/:id', component: BoardComponent }
     ]
-  }
+  },
+  { path: '**', redirectTo: 'main/overview' }
 ];
 
 @NgModule({
